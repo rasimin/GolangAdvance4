525b4fc6d396ec5fd673a0a8019f28b3000cf088
rasimin <rasimin@praisindo.co.id>
2024-07-09 11:47:53 +0700

    pembuatan gateaway

--- main.go
+++ main.go
@@ -1,23 +1,33 @@
 package main
 
 import (
-	"advance2/handler"
+	// "advance2/handler"
 	"advance2/repository/postgres_gorm"
 	"advance2/repository/postgres_pgx"
-	"advance2/router"
+	"net"
+	"time"
+
+	// "advance2/router"
 	"advance2/service"
 	"context"
 	"log"
 
+	//"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/gin"
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"github.com/jackc/pgx/v5/pgxpool"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+
+	grpcHandler "advance2/handler/grpc"
+	pb "advance2/proto/user_service/v1"
 )
 
 func main() {
-	gin.SetMode(gin.ReleaseMode)
-	r := gin.Default()
+	//gin.SetMode(gin.ReleaseMode)
+	//r := gin.Default()
 
 	// setup database connection
 	//dsn := "postgresql://postgres:postgres@localhost:5432/postgres"
@@ -35,26 +45,53 @@ func main() {
 	}
 	// setup service
 
-	// slice db is disabled. uncomment to enabled
-	// var mockUserDBInSlice []entity.User
-	// _ = slice.NewUserRepository(mockUserDBInSlice)
-
-	// uncomment to use postgres pgx
-	// userRepo := postgres_pgx.NewUserRepository(pgxPool)
-
 	// uncomment to use postgres gorm
 	userRepo := postgres_gorm.NewUserRepository(gormDB)
-
-	// service and handler declaration
 	userService := service.NewUserService(userRepo)
-	userHandler := handler.NewUserHandler(userService)
+	//userHandler := ginHandler.NewUserHandler(userService)
+	userHandler := grpcHandler.NewUserHandler(userService)
 
-	// Routes
-	router.SetupRouter(r, userHandler)
+	// Run the grpc server
+	grpcServer := grpc.NewServer()
+	pb.RegisterUserServiceServer(grpcServer, userHandler)
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	go func() {
+		log.Println("Running grpc server in port :50051")
+		_ = grpcServer.Serve(lis)
+	}()
+	time.Sleep(1 * time.Second)
 
-	// Run the server
-	log.Println("Running server on port 8080")
-	r.Run(":8080")
+	// Run the grpc gateway
+	conn, err := grpc.NewClient(
+		"0.0.0.0:50051",
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+	)
+	if err != nil {
+		log.Fatalln("Failed to dial server:", err)
+	}
+	gwmux := runtime.NewServeMux()
+	if err = pb.RegisterUserServiceHandler(context.Background(), gwmux, conn); err != nil {
+		log.Fatalln("Failed to register gateway:", err)
+	}
+
+	// dengan default http server
+	/*
+		gwServer := &http.Server{
+			Addr:    ":8080",
+			Handler: gwmux,
+		}
+		log.Println("Running grpc gateway server in port :8080")
+		_ = gwServer.ListenAndServe()
+	*/
+
+	// dengan GIN
+	gwServer := gin.Default()
+	gwServer.Group("v1/*{grpc_gateway}").Any("", gin.WrapH(gwmux))
+	log.Println("Running grpc gateway server in port :8080")
+	_ = gwServer.Run()
 }
 
 func connectDB(dbURL string) (postgres_pgx.PgxPoolIface, error) {
